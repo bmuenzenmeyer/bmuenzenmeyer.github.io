9e77877b1c03a9d3e41af24e032a87862bdd081d
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2020-02-03 05:00:10 -0600

    feat(v1): add v1)

---
+++ images/header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>brianmuenzenmeyer.com - i know! surprising it hasn't been taken already</title>
+<style type="text/css">
+A:link 
+{color: green}
+A:visited {color: green}
+body
+{
+background-attachment: fixed;
+background-image: url("images/MinimalG.png");
+background-repeat: no-repeat;
+font-family: "Palatino Linotype"
+}
+td.menu{background:white}
+table.menu
+{
+font-size:100%;
+position:absolute;
+visibility:hidden;
+border-collapse: collapse
+}
+p.intro:first-letter 
+{
+color: green;
+font-size:xx-large
+}
+h1{font-family:"Palatino Linotype"}
+}
+p.rightmargin
+{
+text-align:left;
+margin-right:30%
+}
+}
+</style>
+
+<script type="text/javascript">
+function showmenu(elmnt)
+{
+document.getElementById(elmnt).style.visibility="visible";
+}
+function hidemenu(elmnt)
+{
+document.getElementById(elmnt).style.visibility="hidden";
+}
+</script>
+
+</head>
+
+<body>
+<table>
+	<tr>
+		<td>brianmuenzenmeyer.com</td><td> | </td>
+		<td>
+		<a href="http://www.brianmuenzenmeyer.com">Welcome</a></td><td> | </td>
+		<td onmouseover="showmenu('javascript')" onmouseout="hidemenu('javascript')">
+			<table id="javascript" width="120" class="menu" border="1">
+				<tr><td class="menu"><a href="table.htm">Table</a></td></tr>
+				<tr><td class="menu"><a href="secret.htm">Guessing Game</a></td></tr>
+				<tr><td class="menu"><a href="exercise3table.htm">Positioning</a></td></tr>
+				<tr><td class="menu"><a href="catering.htm">...with Content</a></td></tr>
+				<tr><td class="menu"><a href="final.htm">Final</a></td></tr>
+			</table>
+		<a href="javascript.html">Javascript</a>
+		</td><td> | </td>		
+		<td onmouseover="showmenu('java')" onmouseout="hidemenu('java')">
+			<table id="java" width="120" class="menu" border="1">
+				<tr><td class="menu"><a href="java/namebox.java">Name Box</a></td></tr>
+				<tr><td class="menu"><a href="bank.php">Bank Account</a></td></tr>
+				<tr><td class="menu"><a href="rings.php">Rings</a></td></tr>
+				<tr><td class="menu"><a href="time.php">Time</a></td></tr>
+				<tr><td class="menu"><a href="easter.php">Easter</a></td></tr>
+			</table>
+		<a href="java.html">Java</a>
+		</td><td> | </td>
+		<td>
+		<a href="http://www.mmlunch.com">MM Lunch</a>
+		</td>
+		<td width="30%"></td>
+	</tr>
+	<tr>
+		<td colspan="10"><hr width="80%"/></td>
+	</tr>
+</table>
