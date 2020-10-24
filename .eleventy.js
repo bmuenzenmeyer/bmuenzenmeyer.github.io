857f85771e3c0b61735d5bc8f7fd5f456da2f952
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2020-10-24 06:07:28 -0500

    massive cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,35 @@
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max))
 }
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addLayoutAlias("default", "layouts/default.hbs");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("default", "layouts/default.hbs")
 
   //asset folders
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy("img")
 
   // favicons
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("android-chrome-96x96.png");
-  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
-  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
-  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
-  eleventyConfig.addPassthroughCopy("site.webmanifest");
-  eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg");
+  eleventyConfig.addPassthroughCopy("favicon.ico")
+  eleventyConfig.addPassthroughCopy("android-chrome-96x96.png")
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png")
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png")
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png")
+  eleventyConfig.addPassthroughCopy("site.webmanifest")
+  eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg")
 
   //handlebars
-  eleventyConfig.addShortcode("randomItem", function() {
-    const a = new Array(getRandomInt(3)).fill("");
-    return a.reduce((i, s) => `${s}<div class='item'></div>`, "");
-  });
-
-  eleventyConfig.addHandlebarsHelper("reverseArray", function(arr) {
-    arr.reverse();
-  });
-
-  eleventyConfig.addHandlebarsHelper("ifIn", function(elem, list, options) {
+  eleventyConfig.addHandlebarsHelper("ifIn", function (elem, list, options) {
     if (list.indexOf(elem) > -1) {
-      return options.fn(this);
+      return options.fn(this)
     }
-    return options.inverse(this);
-  });
+    return options.inverse(this)
+  })
 
   return {
     dir: {
       input: "./", // Equivalent to Jekyll's source property
-      output: "./_site" // Equivalent to Jekyll's destination property
-    }
-  };
-};
+      output: "./_site", // Equivalent to Jekyll's destination property
+    },
+  }
+}
